dcda2a76bbda0bd9938f03d458d3392aff28919a
Mohamedlee27 <leonk8070@gmail.com>
2022-05-30 15:54:34 +0300

    added methods to the image class

--- Photoapp/models.py
+++ Photoapp/models.py
@@ -1,10 +1,53 @@
+from email.mime import image
 from django.db import models
 import datetime as dt
 
-class Image(models.Model):
-    pic = CloudinaryField('image')
+class Photo(models.Model):
+    image = CloudinaryField('image')
     title = models.CharField(max_length=60)
-    description = models.TextField()
     category = models.ForeignKey(Category,on_delete=models.DO_NOTHING)
-    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING, null=True)
+    description = models.TextField()
     published = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING, null=True)
+
+    @classmethod
+    def photos(cls):
+        photos = cls.objects.all()
+
+        return photos
+
+    @classmethod
+    def get_by_id(cls, id):
+        photo = cls.objects.filter(id=id)
+
+        return photo
+
+    @classmethod
+    def search_by_location(cls, location_term):
+        photos = cls.objects.filter(location__location=location_term)
+
+        return photos
+
+
+    @classmethod
+    def search_by_category(cls, category_term):
+
+        photos = cls.objects.filter(category__category=category_term)
+
+        return photos
+
+    def update_photo(self):
+        updated_photo = Photo.objects.filter(pic=self.id).update(image=self.image,title=self.title,description=self.description,category=self.category,location=self.location)
+        
+        return updated_photo
+
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        Photo.objects.filter(id=self).delete()
+
+
+
+        
