503cfb9f864916e0e56161040b1918395eef17ca
Mohamedlee27 <leonk8070@gmail.com>
2022-05-30 17:21:00 +0300

    created a photo.html file with functionalities

--- Photoapp/views.py
+++ Photoapp/views.py
@@ -4,6 +4,16 @@ import datetime as dt
 from .models import Photo
 
 
+def photo(request,photo_id):
+    try:
+        photo = Photo.objects.get(id=photo_id)
+
+    except DoesNotExist:
+        raise Http404()
+
+    return render(request,'photo.html',{"photo":photo})
+
+
 def photos(request):
     photos = Photo.photos()
 
@@ -23,3 +33,21 @@ def location_results(request):
         message = "You have not searched a photo yet"
 
         return render(request,'results.html',{"message":message})
+
+def category_results(request):
+
+    if 'photo' in request.GET and request.GET["photo"]:
+        search_term = request.GET.get("photo")
+        searched_photos = Photo.search_by_category(search_term)
+
+        message = f"{search_term}"
+
+        return render(request,'results.html',{"message":message,"photos":searched_photos})
+
+    else:
+        message = "You have not searched a photo yet"
+
+        return render(request,'results.html',{"message":message})
+
+
+
