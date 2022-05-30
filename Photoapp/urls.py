e206548e32a3b14dd17e973d2366612842584941
Mohamedlee27 <leonk8070@gmail.com>
2022-05-30 17:15:54 +0300

    addded search results templates

--- Photoapp/urls.py
+++ Photoapp/urls.py
@@ -8,4 +8,8 @@ from django.contrib import admin
 urlpatterns = [
     url('admin/', admin.site.urls,name='admin'),
     url('',views.photos,name='photos'),
+    url('category/',views.category_results,name='category'),
+    url('location/',views.location_results,name='location'),
+    url('photo/(\d+)/',views.photo,name='photo')
+
 ]
