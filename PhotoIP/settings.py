e618eb05e0b5bdda120a5677db05d0495f93b698
Mohamedlee27 <leonk8070@gmail.com>
2022-05-30 15:43:51 +0300

    created an image class

--- PhotoIP/settings.py
+++ PhotoIP/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'photoapp.apps.PhotoappConfig',
+    'PhotoIP',
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +78,10 @@ WSGI_APPLICATION = 'PhotoIP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photo',
+        'USER': 'postgres',
+    'PASSWORD':'lee123',
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -116,8 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- Photoapp/models.py
+++ Photoapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+class Image(models.Model):
+    pic = CloudinaryField('image')
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    category = models.ForeignKey(Category,on_delete=models.DO_NOTHING)
+    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING, null=True)
+    published = models.DateTimeField(auto_now_add=True)
