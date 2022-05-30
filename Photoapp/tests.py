b520a2c48a8760d8ca0869561616305b00e0a51a
Mohamedlee27 <leonk8070@gmail.com>
2022-05-30 16:43:40 +0300

    added test case for categories

--- Photoapp/tests.py
+++ Photoapp/tests.py
@@ -3,25 +3,53 @@ from .models import Image,Category,Location
 import datetime as dt
 
 
+class LocationTestClass(TestCase):
+
+
+    def setUp(self):
+        self.eldoret = Location(location='eldoret')
+
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.eldoret,Location))
+
+    
+    def test_save_method(self):
+        self.eldoret.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_update_location(self):
+        Location.objects.filter(location='eldoret').update(location='nairobi')
+
+    def tearDown(self):
+        Location.objects.filter(location='eldoret').delete()
+
+
 class CategoryTestClass(TestCase):
 
-    # set up method
+    
     def setUp(self):
-        self.general = Category(category='general')
+        self.main = Category(category='main')
 
-    # test instance
+    
     def test_instance(self):
-        self.assertTrue(isinstance(self.general,Category))
+        self.assertTrue(isinstance(self.main,Category))
 
-    # test the save method
+    
     def test_save_method(self):
-        self.general.save_category()
+        self.main.save_category()
         categorys = Category.objects.all()
         self.assertTrue(len(categorys) > 0)
 
     def tearDown(self):
-        Category.objects.filter(category='general').delete()
+        Category.objects.filter(category='main').delete()
 
     def test_update_category(self):
-        Category.objects.filter(category='general').update(category='health')
+        Category.objects.filter(category='main').update(category='sub')
+
+
+
+
+    
 
